bc06b822c61c3847b6afcf2ba230f4f6909fdd78
hugomendonca98 <hugomendonca9@gmail.com>
2022-10-09 00:14:30 -0300

    Adicionando eslint, prettier e sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello Wrold!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
